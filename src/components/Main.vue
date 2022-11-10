9b5afb7907c78a68b4d3934a72742fe73773453c
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-11-10 18:03:57 +0100

    add Main Component

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,44 @@
 <template>
-    <div>
-        sono main
-    </div>
+    <main>
+        <section id="jumbotron"></section>
+        
+        <section id="secondary-nav">
+            <div class="container">
+                
+            </div>
+        </section>
+    </main>
 </template>
 
 <script>
 
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+@import "../assets/scss/partials/variables.scss";
 
+main {
+    color: $main_color;
+    background-color: #1c1c1c;
+
+    .container {
+        position: relative;
+        padding: 50px 0;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    #jumbotron {
+        background-image: url("../assets/img/jumbotron.jpg");
+        background-repeat: no-repeat;
+        background-position: top center;
+        background-size: cover;
+        height: 400px;
+    }
+
+    #secondary-nav {
+        background-color: $DcBlue;
+    }
+}
 </style>
