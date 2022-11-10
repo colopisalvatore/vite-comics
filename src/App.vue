91a0d778652d9b9dc04a48551acf8492a926fc40
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-11-10 16:48:39 +0100

    Add Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
-<script setup>
+<template>
+  <Header />
+  <Main />
+  <Footer />
+
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Main from './components/Main.vue';
+
+export default {  
+  components: {
+    Header,
+    Footer,
+    Main,
+  },
+
+  data() {
+    return 
+  },
+}
 
 </script>
 
-<template>
-  
-</template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+@import "./assets/scss/general.scss";
 
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        sono footer
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
