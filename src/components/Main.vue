c3d27e06ee9f8fd0899e4115e6d7e2d6b93f1efe
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-11-13 12:17:31 +0100

    add products

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,38 @@
 <template>
     <main>
         <section id="jumbotron"></section>
-        
+        <section id="comics">
+            <div class="container">
+                <div class="currentSeries">
+                    <p>CURRENT SERIES</p>
+                </div>
+                <Comics />
+                <div class="load-more">
+                    <p>LOAD MORE</p>
+                </div>
+            </div>
+        </section>        
         <section id="secondary-nav">
             <div class="container">
-                
+                <Products :products="products" />
             </div>
         </section>
     </main>
 </template>
 
 <script>
+import Products from "./Products.vue";
 
+export default {
+    name: "Main",
+    components: {
+        Products,
+    },
+    props: ["products"],
+};
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 @import "../assets/scss/partials/variables.scss";
 
 main {
@@ -36,6 +54,26 @@ main {
         background-size: cover;
         height: 400px;
     }
+
+        .currentSeries {
+            width: fit-content;
+            padding: 10px;
+            font-weight: bold;
+            background-color: dodgerblue;
+            position: absolute;
+            top: -20px;
+            left: 0;
+            cursor: pointer;
+        }
+    
+        .load-more {
+            width: 200px;
+            text-align: center;
+            padding: 10px;
+            font-weight: bold;
+            background-color: dodgerblue;
+            cursor: pointer;
+        }
 
     #secondary-nav {
         background-color: $DcBlue;
---
+++ src/components/Products.vue
@@ -0,0 +1,82 @@
+<template>
+    <ul>
+        <li v-for="(product, index) in products" :key="index">
+            <img :class="{ resized: product.resized }" :src="('../src/assets/img/' + product.img)" />
+            <a :href="product.url">{{ product.text }}</a>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: "Products",
+    data() {
+        return {
+            products: [
+                {
+                    img: "buy-comics-digital-comics.png",
+                    text: "DIGITAL COMICS",
+                    url: "#",
+                    resized: true,
+                },
+                {
+                    img: "buy-comics-merchandise.png",
+                    text: "DC MERCHANDISE",
+                    url: "#",
+                    resized: false,
+                },
+                {
+                    img: "buy-comics-subscriptions.png",
+                    text: "SUBSCRIPTION",
+                    url: "#",
+                    resized: false,
+                },
+                {
+                    img: "buy-comics-shop-locator.png",
+                    text: "COMIC SHOP LOCATOR",
+                    url: "#",
+                    resized: true,
+                },
+                {
+                    img: "buy-dc-power-visa.svg",
+                    text: "DC POWER VISA",
+                    url: "#",
+                    resized: false,
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style scoped lang="scss">
+@import "../assets/scss/partials/variables.scss";
+
+ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+
+    li {
+        padding: 10px;
+        display: flex;     
+        align-items: center;
+
+        img {
+            width: 50px;
+            height: 56px;
+            margin-right: 10px;
+            color: $main_color;
+        }
+
+        img.resized {
+            width: 40px;
+        }
+
+        a {
+            color: $main_color;
+            font-weight: bolder;
+        }
+    }
+}
+</style>
